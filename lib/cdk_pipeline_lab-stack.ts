59f6be7d2579fde7933da5135d98bdd6fbe90c8f
Takhellambam <tgyanen@3c22fb0a931e.ant.amazon.com>
2022-08-12 12:57:00 -0500

    initial commit2

--- lib/cdk_pipeline_lab-stack.ts
+++ lib/cdk_pipeline_lab-stack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
-export class CdkPipelineStack extends Stack {
+export class CdkPipelineLabStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
